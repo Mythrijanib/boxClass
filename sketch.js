cdac5f952fa41f15e9e37b82ef62a4232b16b7fe
Mythrijanib <83968935+Mythrijanib@users.noreply.github.com>
2021-06-09 15:49:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,18 +4,19 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1;
-
+ var ground;
 function setup(){
     var canvas = createCanvas(400,400);
     engine = Engine.create();
     world = engine.world;
 
     box1 = new Box(200,300,50,50);
+    ground=new Ground(150,360,400,20)
 }
 
 function draw(){
     background(0);
     Engine.update(engine);
     box1.display();
-   
+   ground.display();
 }
